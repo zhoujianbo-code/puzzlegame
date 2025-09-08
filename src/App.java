bf47f95d4b7a64b1d9ecdbc2bb0183e7f7358579
zhoujianbo <1044892360@qq.com>
2025-09-08 18:40:05 +0800

    完成拼图除注册之外的所有功能

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
-import com.itheima.ui.LoginJFrame;
-import com.itheima.ui.RegisterJFrame;
+// import com.itheima.ui.LoginJFrame;
+// import com.itheima.ui.RegisterJFrame;
 import com.itheima.ui.GameJFrame;
 
 public class App {
